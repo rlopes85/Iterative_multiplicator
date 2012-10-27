94a93f060985c6f3572750fc1263527195ccd9d8
Ricardo Lopes <ricflopes@gmail.com>
2012-10-27 23:42:08 +0100

    Test bench reviewed

--- multiplicator/verilog/multiplication_tb.v
+++ multiplicator/verilog/multiplication_tb.v
@@ -1,117 +1,82 @@
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // testbench for multiplication (shift-and-add)
-//
+//  Damir Valput
+//  Ricardo Lopes
 //-------------------------------------------------------------------------------
-
 `timescale 1ns / 1ps
 
 module  multiplication_tb;
 
-    parameter   HALF_PERIOD = 20;
-    parameter   LENGTH = 32;
-    
     reg         clock,
                 reset,
                 start;
-    reg         ready;
-    
-    reg         [LENGTH-1:0]    SRA;
-    reg         [LENGTH-1:0]    SRB, 
-                                X, 
-                                Y;
-    reg         [LENGTH-1:0]    RA;   // D flip-flop
-    reg         [4:0]           counter;  //for counting 32 iterations
-    reg[32:0] res_add;
-    reg[LENGTH-1:0] A, B;
-    reg[63:0] P;
-    
-    wire ld, shld, sh;
+            
+    wire        ready;  //OUTPUTS FROM THE DUT (Device-Under-Test) are WIRES!!!!
     
     
-    multiplication
+    reg [31:0]  A,
+                B;
+    wire[63:0]  P;
+    
+    
+    
     // testbench multiplication
-        multiplication_1(
-        .clock(clock),  // master clock
-        .reset(reset),  // master reset, synchronous, active high
-        .start(start),
-        .ready(ready),
-        .A(A),
-        .B(B),
-        .P(P)
-        );
-    
+    multiplication  multiplication_1(
+                                      .clock(clock),  // master clock
+                                      .reset(reset),  // master reset, synchronous, active high
+                                      .start(start),
+                                      .ready(ready),
+                                      .A(A),
+                                      .B(B),
+                                      .P(P)
+                                     );
+                        
     
     // Initial reset:
-    initial
-    begin
-        A = 0;
-        B = 0;
+    initial begin
+        $dumpfile("tbench.vcd");
+        $dumpvars(0,multiplication_tb);
+        $monitor("clock=%b, reset=%b, A=%b, B=%b, P=%b",
+                  clock, reset, A, B, P);
+        A     = 0;
+        B     = 0;
         start = 1'b0;
-        ready = 1'b1;
-        reset = 1'b0; //clock = #10 1'b0;
-        #3000
-            reset = #105 1'b1;
-        #1007
-            reset <= #100 1'b0;
-    end
+        reset = 1'b0;
+        clock = 1'b0;
     
-    
-    // Generate the master clock:
-    always begin
-        #9.688 clock = ~clock;
-        #9.688 clock = ~clock;
-    end
-    
-    
-    / *
-    // Verification: register data to transmit and compare to nxt data received:
-        always @(negedge send_data)
-    begin
-        datasent = datatosend;
-        @(negedge data_received_ready)
-        if (datasent == datareceived)
-            $display("Sent = received ( %h )", datasent);
-        else
-            $display("ERROR: sent %h, received %h", datasent, datareceived);
-    end
-    * /
-    
-    // Send one byte to the first USART, wait for the end of transmission:
-        task FirstMultiplication;
-    //input [7:0] data;
-    begin
-        #100
-            A = 4'hF0F0; //setting the values for multiplication
-        B = 4'hFF00;
-        @(posedge clock)
+        @(posedge clock);
+            reset = 1'b1;
+            #0.2 reset = 1'b0;
+            
+        @(posedge clock);
+            A = 32'h0000AABC; //setting the values for multiplication
+            B = 32'h00AABBCC;
             start = 1'b1; // starting multiplication
-    
-        #5000 // wait some more time...
-    
-            @(posedge ready)
+            
+        @(posedge clock);   //returning start signal to zero after 1 clock-cycle
             start = 1'b0;
-    
-        @(posedge clock)
-            start = 1'b1;
-    
+        
+        @(posedge ready);
+            begin
+                if (P == (A * B))
+                    $display(" Correct result: Found: %d is equal to expected: %d ", P, A * B);
+                else 
+                    $display(" Wrong result: expected %d, found:%d", A * B, P);
+                
+                
+                $finish;
+            end
     end
-    endtask
     
-    // generate N clock cycles (not used in current testbench)
-        / *
-        task DoNClocks;
-    input[LENGTH-1:0]nclocks;
-    integer i;
-    begin
-        clock = 0;  // clock is a global signal
-        for (i = 0; i < nclocks; i = i + 1)
-        begin
-            #(HALF_PERIOD)clock = ~clock;
-            #(HALF_PERIOD)clock = ~clock;
+    
+    
+     //clock genetator
+        always begin
+          #2 clock = !clock;
         end
-    end
-    endtask * /
-
+    
 endmodule
+
+
 
  
